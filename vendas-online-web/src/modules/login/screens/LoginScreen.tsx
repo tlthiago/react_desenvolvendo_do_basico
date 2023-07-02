349fc9b346aa139f8ae1b97dc6ae391ef92c47f6
Thiago Alves <thiagoluizmachadoalves@gmail.com>
2023-07-02 14:28:43 -0300

    Commit da aula 6

---
+++ vendas-online-web/src/modules/login/screens/LoginScreen.tsx
@@ -0,0 +1,9 @@
+import { BackgroundImage } from "../styles/loginScreen.styles";
+
+const LoginScreen = () => {
+  return (<div>
+    <BackgroundImage src="./background.png"/>
+  </div>);
+};
+
+export default LoginScreen;
